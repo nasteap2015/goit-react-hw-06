806e4998a9e6475613132cc2424a15bdc124462c
nasteap2015 <149483716+nasteap2015@users.noreply.github.com>
2024-07-20 00:47:18 +0200

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,10 +16,6 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
 });
 
 export const persistor = persistStore(store);
